38816493ab555c551d666f31568f6f018294c732
Alex <alex.spiesberger@gmail.com>
2020-12-12 14:59:07 +0100

    Adding first lines from chat.py

---
+++ server/chat.py
@@ -0,0 +1,23 @@
+"""
+Represents and stores info about the chat
+"""
+
+class Chat(object):
+
+    def __init__(self):
+        self.content = []
+
+    def update_chat(self, msg):
+        self.content.append(msg)
+    
+    def get_chat(self):
+        return self.content
+    
+    def __len__(self):
+        return len(self.content)
+
+    def __str__(self):
+        return "".join(self.content)
+
+    def __repr__(self):
+        return str(self)
